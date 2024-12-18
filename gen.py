12c78eb16367e3e1947160a09f91df5074266369
lrosdobutko1 <lrosdobutko@outlook.com>
2024-12-18 15:18:34 -0600

    add mutations to new generations

--- gen.py
+++ gen.py
@@ -21,7 +21,6 @@ def main():
                 random.uniform(0,1000), 
                 )
             )
-        
     for i in range(10000):
         ranked_solutions = []
         for s in solutions:
@@ -40,6 +39,30 @@ def main():
             
         print(f"=== Best solutions in generation {i} ===")
         print(ranked_solutions[0])
-
+        
+        if ranked_solutions[0][0] > 9999:
+            break
+        
+        best_solutions = ranked_solutions[:100]
+        
+        elements_0 = []
+        elements_1 = []
+        elements_2 = []
+        for element in best_solutions:
+            elements_0.append(element[1][0])
+            elements_1.append(element[1][1])
+            elements_2.append(element[1][2])
+        
+        new_generation = []
+        for i in range(1000):
+            e1 = random.choice(elements_0) * random.uniform(0.99, 1.01)
+            e2 = random.choice(elements_1) * random.uniform(0.99, 1.01)
+            e3 = random.choice(elements_2) * random.uniform(0.99, 1.01)
+            
+            new_generation.append((e1, e2, e3))
+            
+        solutions = new_generation
+            
 if __name__ == "__main__":
     main()
+
