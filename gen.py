59a054db3a182a5af188bf1c253e96118ae71d0b
lrosdobutko1 <lrosdobutko@outlook.com>
2024-12-18 12:58:37 -0600

    create the fitness function

--- gen.py
+++ gen.py
@@ -1,1 +1,13 @@
-print("hello")
+import random
+
+def equation(a, b, c):
+    return 23*a**4 + 4*b**2 * 11*c**3 - 100
+
+def fitness(a, b, c):
+    ans = equation(a, b, c)
+
+    if ans == 0:
+        return 99999
+    else:
+        return abs(1/ans)
+    
