6366ecb7867e4254b5c4ad9f0fa137ba5ae88483
lrosdobutko1 <lrosdobutko@outlook.com>
2024-12-18 14:36:29 -0600

    add fitness function into the main algo

--- gen.py
+++ gen.py
@@ -22,7 +22,24 @@ def main():
                 )
             )
         
-    print(solutions[:5])
+    for i in range(10000):
+        ranked_solutions = []
+        for s in solutions:
+            ranked_solutions.append(
+                (
+                    fitness(
+                        s[0], 
+                        s[1],
+                        s[2],
+                        ),
+                    s
+                    )
+                )
+            
+            ranked_solutions.sort(reverse=True)
+            
+        print(f"=== Best solutions in generation {i} ===")
+        print(ranked_solutions[0])
 
 if __name__ == "__main__":
     main()
